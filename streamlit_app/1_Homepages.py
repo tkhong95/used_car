0266695e5c2fb920b909b8c117089e183b70ce3a
Trang Khong <minhtrang.khtn@gmail.com>
2023-11-03 16:07:16 -0500

    working on recommendation

---
+++ streamlit_app/1_Homepages.py
@@ -0,0 +1,12 @@
+import streamlit as st
+
+st.set_page_config(
+    page_title="Used Car App"
+)
+
+st.title("Are you looking to buy or sell a used car?")
+st.sidebar.success("Select an option above.")
+
+st.write("This app will help you on making a decision on buying or selling a car")
+
+st.write("After covid pandemic, the price of used car is increasing faster")
--- streamlit_app/pages/2_Used_Car_Price.py
+++ streamlit_app/pages/2_Used_Car_Price.py
@@ -32,17 +32,16 @@ def user_input_features():
                                                   '4 cylinders','5 cylinders', '6 cylinders', 
                                                   '8 cylinders', 'other'))
     fuel = st.sidebar.selectbox('Type of Fuel',('diesel', 'electric', 'gas', 'hybrid', 'other'))
-    odometer = st.sidebar.number_input('Insert a number', min_value=0, max_value=1000000)
+    odometer = st.sidebar.number_input('Car Mileage', min_value=0, max_value=1000000)
     title_status = st.sidebar.selectbox('Current Title Status of Car',('clean', 'lien', 
                                                                        'missing', 'parts only', 
                                                                        'rebuilt', 'salvage'))
     transmission = st.sidebar.selectbox('Type of Transmission',('automatic', 'manual', 'other'))
     drive = st.sidebar.selectbox('Type of car drive',('4WD','FWD','RWD'))
-    #drive = '4wd'
     size = st.sidebar.selectbox('Car Size',('Compact', 'Full-size', 'Mid-size', 'Sub-compact'))
-    types = st.sidebar.selectbox('Type of Car',('SUV', 'Sedan', 'Truck', 'Van', 'Mini-van',
-                                                'Convertible', 'Coupe', 'Hatchback', 'Offroad', 
-                                                'Bus','Pickup', 'Wagon', 'Other'))
+    types = st.sidebar.selectbox('Type of Car',('SUV', 'sedan', 'truck', 'van', 'mini-van',
+                                                'convertible', 'coupe', 'hatchback', 'offroad', 
+                                                'bus','pickup', 'wagon', 'other'))
     paint_color = st.sidebar.selectbox('Car Color', ('Black', 'Blue', 'Brown','Custom'
                                                      ,'Green','Grey', 'Orange','Purple', 
                                                      'Red', 'Silver', 'White', 'Yellow'))
@@ -62,7 +61,7 @@ def user_input_features():
             'transmission': transmission.lower(),
             'drive': drive.lower(),
             'size': size.lower(),
-            'type': types.lower(),
+            'type': types,
             'paint_color': paint_color.lower(),
             'state': state.lower()}
     features = pd.DataFrame(data, index=[0])
@@ -70,73 +69,75 @@ def user_input_features():
 input_df = user_input_features()
 
 
-with open('etr_pipe.pkl',"rb") as f:
+with open('etr_pipe_price.pkl',"rb") as f:
     model = pickle.load(f)
     
 predicttion = model.predict(input_df)
 st.subheader("Predicted Used Car Value")
-st.write(f"${predicttion}")
+st.write(f"${predicttion[0]}")
 
 # Recommendation system
 
-st.write('What used car is ')
+# st.write('What used car is ')
 
 df = pd.read_csv('df_model.csv')
 df.head()
 
-def brand_recommender(manufacturer,year,car_type,price_range):
-    data = df.loc[(df['year']==year) & (df['type']==car_type)
-                 & ((df['price']>=price_range[0]) & (df['price']<=price_range[1]))]
-    data.reset_index(level =0, inplace = True)
+# def brand_recommender(manufacturer,year,car_type,price_range):
+#     data = df.loc[(df['year']==year) & (df['type']==car_type)
+#                  & ((df['price']>=price_range[0]) & (df['price']<=price_range[1]))]
+#     data.reset_index(level =0, inplace = True)
     
-    indices = pd.Series(data.index, index=data['manufacturer'])
+#     indices = pd.Series(data.index, index=data['manufacturer'])
     
-    # Converting the car manufacturer into vectors unigrams and bigrams
-    cvec = CountVectorizer(ngram_range=(1, 2), stop_words='english')
-    cvec_matrix = cvec.fit_transform(data['manufacturer'])
+#     # Converting the car manufacturer into vectors unigrams and bigrams
+#     cvec = CountVectorizer(ngram_range=(1, 2), stop_words='english')
+#     cvec_matrix = cvec.fit_transform(data['manufacturer'])
     
-    # Apply Cosine Similarity
-    cs = cosine_similarity(cvec_matrix)
+#     # Apply Cosine Similarity
+#     cs = cosine_similarity(cvec_matrix)
     
-    # Sort by manufacturer and get the top 6 car with highest similiraty score
-    score = sorted(list(enumerate(cs[indices[manufacturer]])), reverse=True)[0:6]
+#     # Sort by manufacturer and get the top 6 car with highest similiraty score
+#     score = sorted(list(enumerate(cs[indices[manufacturer]])), reverse=True)[0:6]
 
-    # car indicies
-    car_indices = [i[0] for i in score]
+#     # car indicies
+#     car_indices = [i[0] for i in score]
    
-    # Top 6 car recommendations
-    car_recommend = data[['price','manufacturer','type','year','condition','fuel','title_status',
-                'transmission','paint_color']].iloc[car_indices]
-    return car_recommend
+#     # Top 6 car recommendations
+#     car_recommend = data[['price','manufacturer','type','year','condition','fuel','title_status',
+#                 'transmission','paint_color']].iloc[car_indices]
+#     return car_recommend
 
-def user_input():
-    manufacturer = st.sidebar.selectbox('Brands Alphabetically',('Acura', 'Alfa-Romeo', 
-       'Aston-Martin', 'Audi', 'BMW', 'Buick',
-       'Cadillac', 'Chevrolet', 'Chrysler', 'Datsun', 'Dodge', 'Ferrari',
-       'Fiat', 'Ford', 'Gmc', 'Harley-Davidson', 'Honda', 'Hyundai',
-       'Infiniti', 'Jaguar', 'Jeep', 'Kia', 'Land Rover', 'Lexus',
-       'Lincoln', 'Mazda', 'Mercedes-Benz', 'Mercury', 'Mini',
-       'Mitsubishi', 'Morgan', 'Nissan', 'Pontiac', 'Porsche', 'Ram',
-       'Saturn', 'Subaru', 'Tesla', 'Toyota', 'Volkswagen',
-       'Volvo'))
-    year = st.sidebar.selectbox('Year', list(reversed(range(1900,2022))))
-    types = st.sidebar.selectbox('Type of Car',('SUV', 'sedan', 'truck', 'van', 'mini-van',
-                                                'convertible', 'coupe', 'hatchback', 'offroad', 
-                                                'bus','pickup', 'wagon', 'other'))
+# def rec_user_input():
+#     manufacturer = st.sidebar.selectbox('Brands Alphabetically',('Acura', 'Alfa-Romeo', 
+#        'Aston-Martin', 'Audi', 'BMW', 'Buick',
+#        'Cadillac', 'Chevrolet', 'Chrysler', 'Datsun', 'Dodge', 'Ferrari',
+#        'Fiat', 'Ford', 'Gmc', 'Harley-Davidson', 'Honda', 'Hyundai',
+#        'Infiniti', 'Jaguar', 'Jeep', 'Kia', 'Land Rover', 'Lexus',
+#        'Lincoln', 'Mazda', 'Mercedes-Benz', 'Mercury', 'Mini',
+#        'Mitsubishi', 'Morgan', 'Nissan', 'Pontiac', 'Porsche', 'Ram',
+#        'Saturn', 'Subaru', 'Tesla', 'Toyota', 'Volkswagen',
+#        'Volvo'))
+#     year = st.sidebar.selectbox('Year', list(reversed(range(1900,2022))))
+#     types = st.sidebar.selectbox('Type of Car',('SUV', 'sedan', 'truck', 'van', 'mini-van',
+#                                                 'convertible', 'coupe', 'hatchback', 'offroad', 
+#                                                 'bus','pickup', 'wagon', 'other'))
 
 
-# st.write(rec)
+# # st.write(rec)
 
-# import streamlit as st
+# # import streamlit as st
 
-# # Create a Streamlit app
-# st.title("Range of Numbers Input")
+# # # Create a Streamlit app
+# # st.title("Range of Numbers Input")
 
-# # Define the input range
-# min_value = st.number_input("Min Value", value=0)
-# max_value = st.number_input("Max Value", value=100)
+# # # Define the input range
+# # min_value = st.number_input("Min Value", value=0)
+# # max_value = st.number_input("Max Value", value=100)
 
-# # Display the selected range
-# st.write(f"You selected a range from {min_value} to {max_value}")
+# # # Display the selected range
+# # st.write(f"You selected a range from {min_value} to {max_value}")
 
-st.balloons()
+# st.balloons()
+
+
